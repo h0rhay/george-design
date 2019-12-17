50df5249e320e5c291bb6473026c9eda2f5b91e1
George Clark <clarkgeorge76@gmail.com>
2019-12-17 08:33:06 +0000

    source files

---
+++ src/components/layout.js
@@ -0,0 +1,29 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+
+import "./layout.css"
+
+const Layout = ({ children }) => {
+
+  return (
+    <>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()} <span role='img' aria-label='foot emoji'>🥾</span>
+      </footer>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
